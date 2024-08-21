62a6365ecc6c3bbf5a07f71820d562e7831a8038
NdeyeMbombeCisse <ndeyecisse188@gmail.com>
2024-08-21 21:10:37 +0000

    gestion de l'authentification

--- src/app/User/auth/login/login.component.ts
+++ src/app/User/auth/login/login.component.ts
@@ -4,11 +4,12 @@ import { Component, inject } from '@angular/core';
 import { UserService } from '../../user.service';
 import { Router } from '@angular/router';
 import { UserModel } from '../../user.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
@@ -24,13 +25,21 @@ userObjet:UserModel = {}
 
 connexion(){
   console.log(this.userObjet);
-  // if(this.userObjet.email && this.userObjet.password){
-  //   this.userService.login(this.userObjet).subscribe(
-  //     (response:any)=>{
-  //       console.log(response);
-  //       this.router.navigate(['/home'])
-  //     }
-  //   )
+  if(this.userObjet.email && this.userObjet.password){
+    this.userService.login(this.userObjet).subscribe(
+      (response:any)=>{
+        console.log(response.token);
+        if(response.token){
+          localStorage.setItem("token", JSON.stringify(response.token));
+          // verifie le role de l'utilisateur
+          this.router.navigateByUrl("dashboard-admin")
+
+        }
+      }
+    )
   }
 
-}
+}}
+
+
+
