b95782f80c8b6b5e02c92e21299b924b4f4af825
NdeyeMbombeCisse <ndeyecisse188@gmail.com>
2024-08-22 18:21:48 +0000

    affichage des projets

--- src/app/Habitant/composants/liste-projets-habitant/liste-projets-habitant.component.ts
+++ src/app/Habitant/composants/liste-projets-habitant/liste-projets-habitant.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { projetModel } from '../../projet.model';
+import { ProjetService } from '../../projet.service';
 
 @Component({
   selector: 'app-liste-projets-habitant',
@@ -7,6 +9,33 @@ import { Component } from '@angular/core';
   templateUrl: './liste-projets-habitant.component.html',
   styleUrl: './liste-projets-habitant.component.css'
 })
-export class ListeProjetsHabitantComponent {
+export class ListeProjetsHabitantComponent implements OnInit {
+
+    // injection de dependance
+    private projetService = inject(ProjetService)
+  // declaration des variables
+  tabprojet:projetModel[]=[];
+  projetObject:projetModel ={}
+  ngOnInit(): void {
+    this.fetchprojet();
+
+  }
+
+  // fonction pour la recuperation des projets
+fetchprojet(){
+  this.projetService.getAllProjet().subscribe(
+    (response:any)=>{
+      console.log(response);
+
+    //   if(response.data){
+    //     this.tabprojet = response.data;
+    //   }
+      
+    // },(error:any)=>{
+    //   console.log(error);
+    }
+  )
+}
+  
 
 }
--- src/app/Habitant/projet.service.ts
+++ src/app/Habitant/projet.service.ts
@@ -1,9 +1,23 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { apiUrl } from '../apiUrl';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjetService {
 
-  constructor() { }
+  private http = inject(HttpClient);
+  
+
+  // methode pour creer un projet
+  createProjet(projet:any){
+      return this.http.post(`${apiUrl}/add/projets`,projet);
+
+  }
+
+  // methode pour la recuperatio des Projets
+  getAllProjet(){
+      return this.http.get(`${apiUrl}/projets`);
+  }
 }
