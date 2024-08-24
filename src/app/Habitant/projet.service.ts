3005eba5021f14e229730206357a4a04c59662d7
NdeyeMbombeCisse <ndeyecisse188@gmail.com>
2024-08-24 03:56:37 +0000

    gestion des fonctionnalites de l'ahbitants

--- src/app/Habitant/projet.service.ts
+++ src/app/Habitant/projet.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { apiUrl } from '../apiUrl';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -22,11 +23,21 @@ export class ProjetService {
     return this.http.get(`${apiUrl}/projets/publies`,projet)
   }
 
+
+  getPublishedProjectsByUser(userId: number) {
+    return this.http.get(`${apiUrl}/projets/publies/${userId}`);
+  }
+
+
   // publier un projet
 
-  publierProjet(id: number, statut: boolean) {
-    return this.http.patch(`${apiUrl}/projets/${id}/statut`, { statut });
+  publierProjet(id: any, statut: boolean) {
+    return this.http.patch(`${apiUrl}/projets-statut/${id}`, { statut });
   }
+
+ 
+
+
 
   // afiicher les projets pas statut
   getProjetBySttut(statut:any){
@@ -35,11 +46,42 @@ export class ProjetService {
     
   }
 
-
-
-
   // methode pour la recuperatio des Projets
   getAllProjets(){
       return this.http.get(`${apiUrl}/projets`);
   }
+
+   // Méthode pour obtenir le nombre total de projets
+  getTotalProjets() {
+    return this.http.get(`${apiUrl}/projets/count`);
+  }
+
+  // // Méthode pour obtenir le nombre de projets publiés
+  getProjetsPublies() {
+    return this.http.get(`${apiUrl}/projets/publies/count`);
+  }
+
+  // methode pour l'affichage des deatils d'un projets
+
+  getProjetById(id: number) {
+    return this.http.get(`${apiUrl}/details/projet/${id}`);
+  }
+
+
+  // supprimer
+  deleteProject(id: number) {
+    return this.http.delete(`${apiUrl}/delete/projets/${id}`);
+  }
+
+  
+
+
+  
+
+
+ 
+
+
 }
+
+
